3e9d7975d04ee75fc4cb26f0a3e8760f308734a5
Ahmad Faisal <ahmad100.fa100@gmail.com>
2026-06-26 11:52:53 +0300

    Add project files.

---
+++ Entity_framework_read/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = configuration.GetSection("connectionString").Value;
+
+Console.WriteLine(connectionString);
+
+using var conn = new SqlConnection(connectionString);
+
+var SQL = "SELECT * FROM dbo.Wallets";
+
+using SqlCommand command = new SqlCommand(SQL, conn);
+command.CommandType = CommandType.Text;
+
+conn.Open();
+
+using SqlDataReader reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+    Wallet wallet = new Wallet
+    {
+        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        Holder = reader.GetString(reader.GetOrdinal("Holder")),
+        Balance = reader.GetDecimal(reader.GetOrdinal("Balance"))
+    };
+
+    Console.WriteLine(wallet);
+}
---
+++ Entity_framework_write/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = configuration.GetSection("connectionString").Value;
+
+Console.WriteLine(connectionString);
+
+using var conn = new SqlConnection(connectionString);
+
+var walletToInsert = new Wallet {
+    Holder = "Mona",
+    Balance = 2000
+};
+
+var SQL = "Insert into WALLETS (Holder, Balance) values " +
+    $"(@Holder, @Balance) ";
+
+SqlParameter holderParameter = new SqlParameter
+{
+    ParameterName = "@Holder",
+    SqlDbType = SqlDbType.VarChar,
+    Direction = ParameterDirection.Input,
+    Value = walletToInsert.Holder
+};
+
+SqlParameter BalanceParameter = new SqlParameter
+{
+    ParameterName = "@Balance",
+    SqlDbType = SqlDbType.Decimal,
+    Direction = ParameterDirection.Input,
+    Value = walletToInsert.Balance
+};
+
+
+using SqlCommand command = new SqlCommand(SQL, conn);
+
+command.Parameters.Add(holderParameter);
+command.Parameters.Add(BalanceParameter);
+
+command.CommandType = CommandType.Text;
+
+conn.Open();
+
+if (command.ExecuteNonQuery() > 0) {
+
+    Console.WriteLine($"wallet for {walletToInsert.Holder} added successfully");
+}
+else {
+    Console.WriteLine("falied to add the row");
+}
+
+    conn.Close();
+
+Console.ReadKey();
+
